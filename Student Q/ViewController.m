9934422ebbb81ce05c0e4f26baa30fb0b2242064
Eric Mentele <diviningsage@gmail.com>
2015-03-26 03:12:54 -0700

    Table View added.

--- Student Q/ViewController.m
+++ Student Q/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 #define studentQURL @"https://studentq.firebaseio.com"
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,12 +20,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+//+++++++++++++++++++++++++++++++FIREBASE++++++++++++++++++++++++++++++++++++++++
     // Do any additional setup after loading the view, typically from a nib.
     self.firebase = [[Firebase alloc] initWithUrl:studentQURL];
     // Create a reference to a Firebase location
-    
     [_firebase setValue:@"Do you have data? You'll love Firebase."];
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    self.tableView.dataSource = self;
+    
 }
+
+
+#pragma mark Table View methods
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 5;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"studentCell"];
+    cell.textLabel.text = @"Test";
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
