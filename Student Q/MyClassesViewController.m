5b6c7b1cc62b7f0556b5dc606a314bce3be0f537
Eric Mentele <diviningsage@gmail.com>
2015-04-07 14:52:38 -0700

    Log in working.

--- Student Q/MyClassesViewController.m
+++ Student Q/MyClassesViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "MyClassesViewController.h"
+#import "NewUserViewController.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
+
+#define studentQURL @"https://studentq.firebaseio.com"
 
 @interface MyClassesViewController ()
 
+@property (nonatomic, strong) FAuthData *currentUser;
 @end
 
 @implementation MyClassesViewController
@@ -17,6 +22,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.firebase                    = [[Firebase alloc] initWithUrl:studentQURL];
+    
+    NSString *email                  = [[NSUserDefaults standardUserDefaults]stringForKey:@"email"];
+    NSString *password               = [[NSUserDefaults standardUserDefaults]stringForKey:@"password"];
+    
+    [self.firebase authUser:email password:password withCompletionBlock:^(NSError *error, FAuthData *authData) {
+        
+        if (authData == nil) {
+            
+            [self performSegueWithIdentifier:@"newUser" sender:self];
+
+        }
+        else if (error) {
+             DDLogError(@"%@", error);
+            
+        } else {
+            DDLogInfo(@"Logged in!");
+        }
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,13 +50,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
