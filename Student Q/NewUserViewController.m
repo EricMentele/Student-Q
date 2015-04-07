35676a31dbfca9e593deb1f80fc1acbdfbc75977
Eric Mentele <diviningsage@gmail.com>
2015-04-06 23:06:35 -0700

    User creation added.

--- Student Q/NewUserViewController.m
+++ Student Q/NewUserViewController.m
@@ -24,92 +24,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.firebase               = [[Firebase alloc] initWithUrl:studentQURL];
-    [self.firebase setValue:nil];
-    
-    NSString *UUID              = [[NSUUID UUID] UUIDString];
-    NSString *email             = [NSString stringWithFormat:@"eric.mentele.%@@icloud.com", UUID];
-    
-    [self.firebase createUser:email password:@"password"
-     withValueCompletionBlock:^(NSError *error, NSDictionary *authUser) {
-         
-         // check to see if account was created ok
-         if (error) {
-             DDLogError(@"%@", error);
-         } else {
-             NSLog(@"Successfully created user account with uid: %@", authUser[@"uid"]);
-         }
-         DDLogInfo(@"email %@", email);
-         // add profile info for the newly created user
-         Firebase *users             = [self.firebase childByAppendingPath:@"users"];
-         Firebase *user1             = [users childByAutoId];
-         NSDictionary *profile1       = @{
-                                         @"userID"         : authUser[@"uid"],
-                                         @"fullName"       : @"Eric Mentele",
-                                         @"profilePicture" : @"some path to picture"
-                                         };
-         [user1 setValue:profile1];
-         
-         //add a second userX
-         Firebase *user2             = [users childByAutoId];
-         NSDictionary *profile2      = @{
-                                         @"userID"         : authUser[@"uid"],
-                                         @"fullName"       : @"Hot Dog",
-                                         @"profilePicture" : @"path to picture"
-                                         };
-         [user2 setValue:profile2];
-         
-         // add class to be reused by any user
-         Firebase *classes           = [self.firebase childByAppendingPath:@"classes"];
-         Firebase *class             = [classes childByAutoId];
-         NSDictionary *classInfo     = @{
-                                         @"className"  : @"iOS Fundamentals",
-                                         @"location" : @"Redmond, WA",
-                                         @"classOwnerID" : authUser[@"uid"],
-                                         @"taCode" : @"5678",
-                                         @"studentCode": @"5642",
-                                         @"helpQueue" : @{
-                                        
-                                                 
-                                                        }
-                                         };
-         [class setValue:classInfo];
-         
-         // add user created above as teacher of the newly created class
-         Firebase *userClasses       = [user1 childByAppendingPath:@"classes"];
-         Firebase *userClass         = [userClasses childByAppendingPath:class.key];
-         NSDictionary *userClassInfo = @{
-                                         @"classRole": @"Teacher",
-                                         @"presence": @(1)
-                                         };
-         [userClass setValue:userClassInfo];
-         
-         Firebase *user2Classes       = [user2 childByAppendingPath:@"classes"];
-         Firebase *user2Class         = [user2Classes childByAppendingPath:class.key];
-         NSDictionary *user2ClassInfo = @{
-                                         @"classRole": @"Student",
-                                         @"presence": @(1)
-                                         };
-         [user2Class setValue:user2ClassInfo];
-         
-         // add second user
-         NSDictionary *queueStudent   = @{
-                                          @"userID":user1.key
-                                         };
-         
-         // queue second user for help in class
-         Firebase *classQueue         = [class childByAppendingPath:@"helpQueue"];
-          Firebase *addStudent             = [classQueue childByAutoId];
-         [addStudent setValue:queueStudent];
-     }];
+    self.firebase                          = [[Firebase alloc] initWithUrl:studentQURL];
     
 }
 
-#pragma mark Add Student To Queue Methods
+
+#pragma mark Keyboard handling
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardOn:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardOff:) name:UIKeyboardWillHideNotification object:nil];
+}
 
 
+- (void)viewWillDisappear:(BOOL)animated {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
 
+
+- (void)keyboardOn:(NSNotification*)notification {
+    
+    UIBarButtonItem *doneButton            = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(keyboardOff:)];
+    self.navigationItem.rightBarButtonItem = doneButton;
+}
+
+
+- (void)keyboardOff:(NSNotificationCenter*)notification {
+    
+    [self.view endEditing:YES];
+}
+
+
+#pragma mark New User
 - (IBAction)addUser:(id)sender {
+    
+    [self.firebase createUser:self.emailTextField.text password:self.passwordTextField.text withCompletionBlock:^(NSError *error) {
+        
+        if (error) {
+            DDLogError(@"%@", error);
+        } else {
+            NSLog(@"Successfully created user account!");
+        }
+        //DDLogInfo(@"email %@", email);
+        // add profile info for the newly created user
+        Firebase *users                        = [self.firebase childByAppendingPath:@"users"];
+        Firebase *user                         = [users childByAutoId];
+        NSDictionary *profile                  = @{
+                                                   @"userID"         : user.key,
+                                                   @"fullName"       : self.nameTextField.text,
+                                                   @"profilePicture" : @"some path to picture"
+                                                   };
+        [user setValue:profile];
+    }];
     
 }
 
---
+++ Student Q/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Student Q
+//
+//  Created by Eric Mentele on 4/6/15.
+//  Copyright (c) 2015 Eric Mentele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
